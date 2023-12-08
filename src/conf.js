9a4ad20d660fa5052bdbfa60b0653007590e9650
ShankerTalluri <bhavanishankarbst@gmail.com>
2023-12-08 16:26:15 +0530

    appwrite env setup

---
+++ src/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteEndpoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
