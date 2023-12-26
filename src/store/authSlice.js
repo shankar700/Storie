ded0a678ec0b5b128e350ebc1b4931eb64b51436
ShankerTalluri <bhavanishankarbst@gmail.com>
2023-12-26 12:37:35 +0530

    added notes

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,13 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// initial state for the slice
 const initialState = {
   status: false,
   userData: null,
 };
 
+// createSlice takes three params, name(sliceName), initalState and reducer functions
 const authSlice = createSlice({
   name: "auth",
   initialState,
+  // reducer functions are called "actions" and they have access to "state" and "action" from where we can get payload
   reducers: {
     login: (state, action) => {
       state.status = true;
@@ -19,6 +22,7 @@ const authSlice = createSlice({
   },
 });
 
+  // actions have to exported individually, these actions are automatically created by createSlice method
 export const { login, logout } = authSlice.actions;
-
+// eventhough it's declared as reducers in the slice, it is accessed by <sliceName>."reducer"
 export default authSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+// here we are accessing authSlice.reducer from ./authSlice
 import authReducer from "./authSlice"
 
 const store = configureStore({
